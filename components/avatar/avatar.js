0a049469be68fb8055d67df3253db843e2a83e26
toshinari123 <68701054+toshinari123@users.noreply.github.com>
2023-08-05 22:57:50 +0800

    first commit yey

---
+++ components/avatar/avatar.js
@@ -0,0 +1,25 @@
+class Avatar extends HTMLElement {
+  #shadow;
+  constructor() {
+    super();
+    this.#shadow = this.attachShadow({ mode: 'closed' });
+    this.#render();
+  }
+  async #render() {
+    const avatarCSS = document.createElement("link");
+    avatarCSS.setAttribute("href","avatar.css");
+    avatarCSS.setAttribute("rel","stylesheet");
+    avatarCSS.setAttribute("type","text/css");
+    this.#shadow.appendChild(avatarCSS);
+    const img = document.createElement("img");
+    img.setAttribute("src", this.getAttribute("src"));
+    this.#shadow.appendChild(img);
+    this.#defineCallBacks();
+    return;
+  }
+  #defineCallBacks(){
+    return;
+  }
+}
+
+export {Avatar}
